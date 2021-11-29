468fc16265a834921a488745185f16ade264fde8
Strawing <liyafe1997@126.com>
2021-11-29 01:03:05 +0000

    Implemented scroll menu

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ def DownloadCityData(url):
 def ShowCurrentWeather(stdscr, selected_city):
     stdscr.erase()
     stdscr.refresh()
-    #print(selected_city)
+    # print(selected_city)
     stdscr.addstr(selected_city["name"] + " Current Weather:\n")
-    json_data = json.loads(requests.get("https://api.openweathermap.org/data/2.5/weather?id=" + str(selected_city["id"]) + "&appid=93c4fc843f2189f3232b27952027013b&units=metric").content)
+    json_data = json.loads(requests.get("https://api.openweathermap.org/data/2.5/weather?id=" + str(
+        selected_city["id"]) + "&appid=93c4fc843f2189f3232b27952027013b&units=metric").content)
     temp_min = str(int(float(json_data["main"]["temp_min"])))
     temp_max = str(int(float(json_data["main"]["temp_max"])))
     humidity = str(int(float(json_data["main"]["humidity"])))
@@ -29,18 +30,51 @@ def ShowCurrentWeather(stdscr, selected_city):
             weather = weather + " and "
         weather += j["main"]
     try:
-        stdscr.addstr("  " + weather + ", " + temp_min + "℃  to " + temp_max + "℃ ," + "  Humidity:" + humidity + "%")
+        stdscr.addstr("  " + weather + ", " + temp_min + "℃  to " +
+                      temp_max + "℃ ," + "  Humidity:" + humidity + "%")
         stdscr.addstr("\n")
     except:
         pass
-    c = stdscr.getch()
+    stdscr.addstr("Please choose an option:\n")
+
+    #Menu options 
+
+    while True:
+        
+        # Page the result
+        max_lines = max_x-4
+        pages = int(len(menu_list)/max_lines)
+        new_max_x, new_max_y = stdscr.getmaxyx()
+        if(new_max_x != max_x):
+            #reflash page when terminal size be changed
+            current_page = 0
+            selected_index = 0
+            max_x = new_max_x
+            max_y= new_max_y
+
+        #display menu
+        if (len(menu_list) != 0):
+            for i in range(0, len(menu_list)):
+                if(int(i/max_lines) == current_page):
+                    try:
+                        if (selected_index == i):
+                            stdscr.addstr("  [->]")
+                            selected_city = menu_list[i]
+                        else:
+                            stdscr.addstr("  [  ]")
+                        stdscr.addstr(
+                            str(i+1) + "." + menu_list[i]["name"] + "\n")
+                    except:
+                        pass
+    
 
 
 def ShowForecast(stdscr, selected_city):
     stdscr.erase()
     stdscr.refresh()
-    #print(selected_city)
-    json_data = json.loads(requests.get("https://api.openweathermap.org/data/2.5/forecast?id=" + str(selected_city["id"]) + "&appid=93c4fc843f2189f3232b27952027013b&units=metric").content)
+    # print(selected_city)
+    json_data = json.loads(requests.get("https://api.openweathermap.org/data/2.5/forecast?id=" + str(
+        selected_city["id"]) + "&appid=93c4fc843f2189f3232b27952027013b&units=metric").content)
     for i in json_data["list"]:
         date = datetime.fromtimestamp(i["dt"]).strftime("%Y-%m-%d")
         temp_min = str(int(float(i["main"]["temp_min"])))
@@ -52,7 +86,8 @@ def ShowForecast(stdscr, selected_city):
                 weather = weather + " and "
             weather += j["main"]
         try:
-            stdscr.addstr(date + ": " + weather + ", " + temp_min + "℃  to " + temp_max + "℃," + "  Humidity:" + humidity + "%")
+            stdscr.addstr(date + ": " + weather + ", " + temp_min +
+                          "℃  to " + temp_max + "℃," + "  Humidity:" + humidity + "%")
             stdscr.addstr("\n")
         except:
             pass
@@ -63,10 +98,10 @@ def ShowForecast(stdscr, selected_city):
     while True:
         if c == curses.KEY_DOWN:
             stdscr.move(0, 0)
-            #stdscr.refresh()
+            # stdscr.refresh()
         elif c == curses.KEY_UP:
             stdscr.move(10, 5)
-            #stdscr.refresh()
+            # stdscr.refresh()
     stdscr.clear()
     stdscr.refresh()
 
@@ -74,26 +109,29 @@ def ShowForecast(stdscr, selected_city):
 def SearchingMenu(stdscr, city_data):
     curses.use_default_colors()
     stdscr.scrollok(True)
-    #stdscr.idlok(True)
-    #stdscr.keypad(True)
+    # stdscr.idlok(True)
+    # stdscr.keypad(True)
     input_string = ""
     input_string_before_cursor = ""
     input_string_after_cursor = ""
     c = 0
-    selected_index = 0
     selected_city = []
+
+    # menu parameters
+    selected_index = 0
+    current_page = 0
+    max_x, max_y = stdscr.getmaxyx()
+    
     while True:
         city_list = []
         city_list_count = 0
         too_many_result_flag = 0
         stdscr.erase()
-
-        # Pages
-        max_x, max_y = stdscr.getmaxyx()
-        stdscr.addstr("max_x=" + str(max_x) + " max_y=" + str(max_y) + "\n")
         
+        #stdscr.addstr("max_x=" + str(max_x) + " max_y=" + str(max_y) + "\n")
         # Search
-        stdscr.addstr("Search for City: " + input_string_before_cursor + "_" + input_string_after_cursor)
+        stdscr.addstr("Search for City: " +
+                      input_string_before_cursor + "_" + input_string_after_cursor)
         stdscr.addstr("\n")
         #stdscr.addstr("InputKey:" + str(c) + "\n")
         input_string = input_string_before_cursor + input_string_after_cursor
@@ -101,24 +139,40 @@ def SearchingMenu(stdscr, city_data):
             stdscr.addstr("City List:\n")
             for i in city_data:
                 if input_string.upper() in i["name"].upper():
-                    if (city_list_count < 10):
+                    if (city_list_count < 20):
                         city_list.append(i)
                         city_list_count += 1
                     else:
                         too_many_result_flag = 1
             if (too_many_result_flag):
-                stdscr.addstr("  Too many result! Only show first 10 result. Please enter more details.\n")
+                stdscr.addstr(
+                    "  Too many result! Only show first 20 result. Please enter more details.\n")
+
+            # Page the result
+            max_lines = max_x-4
+            pages = int(len(city_list)/max_lines)
+            new_max_x, new_max_y = stdscr.getmaxyx()
+            if(new_max_x != max_x):
+                #reflash page when terminal size be changed
+                current_page = 0
+                selected_index = 0
+                max_x = new_max_x
+                max_y= new_max_y
+
+            #display menu
             if (len(city_list) != 0):
                 for i in range(0, len(city_list)):
-                    try:
-                        if (selected_index == i):
-                            stdscr.addstr("  [->]")
-                            selected_city = city_list[i]
-                        else:
-                            stdscr.addstr("  [  ]")
-                        stdscr.addstr(str(i) + "." + city_list[i]["name"] + "\n")
-                    except:
-                        pass
+                    if(int(i/max_lines) == current_page):
+                        try:
+                            if (selected_index == i):
+                                stdscr.addstr("  [->]")
+                                selected_city = city_list[i]
+                            else:
+                                stdscr.addstr("  [  ]")
+                            stdscr.addstr(
+                                str(i+1) + "." + city_list[i]["name"] + "\n")
+                        except:
+                            pass
 
         c = stdscr.getch()
         if ((c >= 33 and c <= 126) or (c >= 128 and c <= 254)):
@@ -130,9 +184,17 @@ def SearchingMenu(stdscr, city_data):
         elif (c == 330):
             input_string_after_cursor = input_string_after_cursor[1:]
         elif (c == curses.KEY_UP and selected_index > 0):
+            # switch page
+            if((selected_index+1) % max_lines == 1):
+                if(current_page > 0):
+                    current_page -= 1
             selected_index -= 1
         elif (c == curses.KEY_DOWN and selected_index < len(city_list) - 1):
             selected_index += 1
+            # switch page
+            if(selected_index % max_lines == 0):
+                current_page += 1
+
         elif (c == curses.KEY_RIGHT):
             if (input_string_after_cursor != ""):
                 input_string_before_cursor += input_string_after_cursor[0]
@@ -140,7 +202,8 @@ def SearchingMenu(stdscr, city_data):
             pass
         elif (c == curses.KEY_LEFT):
             if (input_string_before_cursor != ""):
-                input_string_after_cursor = input_string_before_cursor[-1] + input_string_after_cursor
+                input_string_after_cursor = input_string_before_cursor[-1] + \
+                    input_string_after_cursor
                 input_string_before_cursor = input_string_before_cursor[:-1]
             pass
         elif (c == curses.KEY_ENTER or c == 10):
@@ -152,7 +215,8 @@ def SearchingMenu(stdscr, city_data):
 
 
 if __name__ == "__main__":
-    #city_data = DownloadCityData("http://bulk.openweathermap.org/sample/city.list.json.gz")
-    city_data = DownloadCityData("http://127.0.0.1:8000/city.list.json.gz")
+    city_data = DownloadCityData(
+        "http://bulk.openweathermap.org/sample/city.list.json.gz")
+    #city_data = DownloadCityData("http://127.0.0.1:8000/city.list.json.gz")
     selected_city = curses.wrapper(SearchingMenu, city_data)
     pass
